04c19b37a790891ed0ba19a3392a747f73bf6011
xiaoliuxing520 <125547843+xiaoliuxing520@users.noreply.github.com>
2023-10-08 09:21:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * @package Simplicity
  * @author Canghai
  * @version 1.3
- * @link https://github.com/pickcho/maupassant
+ * @link https://github.com/xiaoliuxing520/Simplicity.git
  */
  $this->need('header.php');
  ?>
